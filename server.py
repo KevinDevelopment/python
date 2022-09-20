0886626d9559dd9320a69ec752a5eb949a3f06cf
Kevin Ferreira <kevincristhian@outlook.com>
2022-09-19 22:46:26 -0300

    :sparkles: feat: add server python

---
+++ server.py
@@ -0,0 +1,11 @@
+# server.py
+from flask import Flask
+
+SECRET_MESSAGE = "fluffy tail"
+app = Flask(__name__)
+
+@app.route("/")
+def get_secret_message():
+    return SECRET_MESSAGE
+
+# para rodar => uwsgi --http-socket 127.0.0.1:5683 --mount /=server:app http://localhost:5683
