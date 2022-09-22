87239b0e656f2acf4a470057047678b369552b23
Kevin Ferreira <kevincristhian@outlook.com>
2022-09-22 19:10:09 -0300

    :sparkles: feat: add symmetric keys

--- client.py
+++ client.py
@@ -1,10 +1,13 @@
 import os
 import requests
 
+
 def get_secret_message():
     url = os.environ["SECRET_URL"]
     response = requests.get(url)
     print(f"The secret message is: {response.text}")
 
+
 if __name__ == "__main__":
     get_secret_message()
+    # SECRET_URL="http://127.0.0.1:5683" python client.py
